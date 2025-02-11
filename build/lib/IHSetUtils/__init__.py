19d5976e3002bdbd2bb1d32ff67de9289348538f
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-02-11 10:35:18 +0100

    updating all

--- build/lib/IHSetUtils/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/__init__.py
-
-# Import modules and functions from your package here
-from .conversion_IHData import *
-from .geometry import * 
-from .morfology import *
-from .waves import *
