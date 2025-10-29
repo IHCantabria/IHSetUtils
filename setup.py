db4f72dd3145a63a32d6f446038696daf3cf4466
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-10-29 10:22:04 +0100

    correcting metrics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetUtils',
-    version='2.6.11',
+    version='2.6.12',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
